63dc698d52443169b401b87f5e9bd0cc26efb7d5
LEWY254 <167128680+LEWY254@users.noreply.github.com>
2024-07-25 03:03:32 +0300

    Add files via upload

---
+++ landingpage.js
@@ -0,0 +1,14 @@
+const tour = document.getElementById("tour");
+tour.addEventListener("click", () => {
+    window.open("timeline.html", "_blank");
+});
+
+const podcast=document.getElementById("podcast")
+podcast.addEventListener("click",()=>{
+    window.open("podcast.html","_self")
+})
+
+const quiz = document.getElementById("quiz");
+quiz.addEventListener("click", () => {
+    window.open("https://www.wisc-online.com/arcade/games/humanities/history/30622/our-independence-back", "_blank");
+})
